46239bba71f92b2c7653cd26131d49e9cc3610a9
satyampyasi <satyampyasi565@gmail.com>
2024-12-05 11:21:46 +0530

    pattern

---
+++ satcoding/basiccodepart4.c/odd2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int num;
+    printf("enter tha number ");
+    scanf("%d",&num);
+    if (num ==num/2*2)  
+      {
+        printf("Even ");
+      }
+    else{
+        printf("odd number");
+    }
+}
